1c91eafbcc6d325282e17c55f35c338190e4f8e7
FabrizioCano <fabriziocano7@gmail.com>
2025-04-28 11:44:46 -0300

    errors solved

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
       useUnifiedTopology: true
     });
 
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
